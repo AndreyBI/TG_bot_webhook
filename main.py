5e197406789bc0a2777e514b709c1441dcfa9dfb
Baykalov Andrey <baykalov.a.i.tt@gmail.com>
2022-08-03 17:45:39 +0700

    First commit

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
 
-TOKEN = os.getenv('5525955265:AAFgKtqEKaiBSwP9YmvyTw9hFfbOoeNaprg')
+TOKEN = os.getenv('Token')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('tg0bot0aiogram0webhook')
+HEROKU_APP_NAME = os.getenv('Name')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
