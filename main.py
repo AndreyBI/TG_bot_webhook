e4779b549b58ea9c34a26af87b39061bb1bdc0c0
Baykalov Andrey <baykalov.a.i.tt@gmail.com>
2022-08-03 18:36:59 +0700

    Change Port

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_PATH = f'/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_PORT = os.getenv(8000)
 
 
 async def on_startup(dispatcher):
